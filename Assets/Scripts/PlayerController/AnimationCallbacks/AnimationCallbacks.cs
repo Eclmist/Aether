885628566198db91902fc99dda8c7b10d4e6734d
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-01-20 04:40:13 +0800

    Made character much less likely to get stuck when jumping
    Renamed 3rdParty -> Extern

---
+++ Assets/Scripts/PlayerController/AnimationCallbacks/AnimationCallbacks.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationCallbacks : MonoBehaviour
+{
+    private Animator m_Animator;
+
+    [SerializeField]
+    private PlayerMovement m_PlayerMovement;
+
+    protected void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    public void JumpStartCompleted()
+    {
+        m_Animator.SetBool("Jumping", false);
+        m_PlayerMovement.Jump();
+    }
+
+    public void OnCallChangeFace(string target)
+    {
+
+    }
+}
