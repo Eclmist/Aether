cdef84165afe56478b62c77e252307ef396e193d
heze8 <46834785+heze8@users.noreply.github.com>
2020-01-27 20:16:20 +0800

    Implement a finite state machine AI, currently with one state: patrolling.I used the NavMeshAgent, and NavMeshbaking for the AI pathfinding, and I've converted the mechanim into a visualy finite state machine so far, by inhereting from StateMachineBehaviour.
    TO USE: Open the GYM_AI scene

---
+++ Assets/Scripts/AI/AIManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiManager : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    private NavMeshAgent agent;
+
+    public void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public GameObject[] Waypoints
+    {
+        get => waypoints;
+        set => waypoints = value;
+    }
+
+    
+}
---
+++ Assets/Scripts/AI/States/Patrol.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+public class Patrol : StateMachineBehaviour
+{
+    [SerializeField]
+    public Transform[] waypoints;
+    private NavMeshAgent agent;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        CheckForAgent(animator);
+        GotoNextPoint();
+    }
+
+    private void CheckForAgent(Animator animator)
+    {
+        if (agent == null)
+        {
+            agent = animator.gameObject.GetComponent<NavMeshAgent>();
+        }
+    }
+    
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        CheckForAgent(animator);
+        
+        if (!agent.pathPending && agent.remainingDistance < 0.1f)
+            GotoNextPoint();
+    }
+
+    private void GotoNextPoint()
+    {
+        Transform destination = waypoints[Random.Range(0, waypoints.Length)];
+        Debug.Log(destination.position);
+        agent.SetDestination(destination.position);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
