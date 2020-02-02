93118b4af6e45d37cca282f7bc770ef3ea137777
heze8 <46834785+heze8@users.noreply.github.com>
2020-02-02 12:00:52 +0800

    Made the flag an AI that runs away from the player

--- Assets/Scripts/CTF/FlagGoal.cs
+++ Assets/Scripts/CTF/FlagGoal.cs
@@ -2,19 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlagCatcher : MonoBehaviour
+[RequireComponent(typeof(Collider))]
+public class FlagGoal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter(Collider c) 
     {
--- Assets/Scripts/CTF/FlagPowerUp.cs
+++ Assets/Scripts/CTF/FlagPowerUp.cs
@@ -2,31 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class FlagPowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter(Collider c) 
     {
         if (c.CompareTag("Player"))
         {
+            SetCapture(c.gameObject);
             FlagManager manager = c.GetComponent<FlagManager>();
 
             if (manager != null && !manager.CheckIfFlagInPosession())
             {
                 manager.SetBool(true);
-                Destroy(gameObject);
+                //Destroy(gameObject);
             }
         }
     }
+
+    private void SetCapture(GameObject player)
+    {
+        GameObject parentGameObject = this.transform.parent.gameObject;
+        parentGameObject.transform.SetParent(player.transform);
+        parentGameObject.GetComponent<AiActor>().SetInactive();
+    }
+
 }
