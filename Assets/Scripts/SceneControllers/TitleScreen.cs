672671aa3f8789c657620c3315918a1f8d69004d
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-02-03 22:26:56 +0800

    Added animations to titlescreen/mainmenu

---
+++ Assets/Scripts/SceneControllers/TitleScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreen : MonoBehaviour
+{
+    [SerializeField]
+    public Animator m_MainCanvasAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && m_MainCanvasAnimator != null)
+        {
+            // Ignore mouse input 
+            if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+                return;
+
+            m_MainCanvasAnimator.SetTrigger("Start");
+        }
+        
+    }
+}
