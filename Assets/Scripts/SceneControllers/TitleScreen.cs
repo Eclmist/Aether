fb459569e2465844cc393927c70867ceedf6ea84
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-02-04 00:10:27 +0800

    Added UI sounds for title screen

--- Assets/Scripts/SceneControllers/TitleScreen.cs
+++ Assets/Scripts/SceneControllers/TitleScreen.cs
@@ -7,17 +7,30 @@ public class TitleScreen : MonoBehaviour
     [SerializeField]
     public Animator m_MainCanvasAnimator;
 
+    private bool m_AnyKeyPressed;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown && m_MainCanvasAnimator != null)
+        if (Input.anyKeyDown && m_MainCanvasAnimator != null && !m_AnyKeyPressed)
         {
             // Ignore mouse input 
             if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
                 return;
 
             m_MainCanvasAnimator.SetTrigger("Start");
+            AudioManager.m_Instance.PlaySoundAtPosition("GEN_Success_2", Camera.main.transform.position, 1.0f, 1.0f);
+            m_AnyKeyPressed = true;
         }
-        
+    }
+
+    public void GoToMultiplayLobby()
+    {
+        AudioManager.m_Instance.PlaySoundAtPosition("GEN_Success_1", Camera.main.transform.position, 1.0f, 1.0f);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
