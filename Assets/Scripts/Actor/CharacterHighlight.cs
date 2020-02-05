4fe2213e5755f01a04ba700398a7473aca75bbd7
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-05 23:56:34 +0800

    fix minimal highlight

---
+++ Assets/Scripts/Actor/CharacterHighlight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHighlight : MonoBehaviour
+{
+    private Renderer[] m_Renderers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Renderers = this.gameObject.GetComponentsInChildren<Renderer>();
+    }
+
+
+    private void Update()
+    {        
+        foreach (Renderer r in m_Renderers)
+        {
+            if (r == null)
+            {
+                continue;
+            } 
+            else
+            {
+                Material[] materials = r.GetComponents<Material>();
+                if (materials != null)
+                {
+                    foreach (Material m in materials)
+                    {
+                        if (m != null)
+                        {
+                            m.SetFloat("_OutlineOpacity", 1);
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+    //private void SetMaterialOpacity()
+    //{
+        //SkinnedMeshRenderer[] childrenRenderers = m_Player.GetComponentsInChildren<SkinnedMeshRenderer>();
+        //Debug.Log(childrenRenderers.Length);
+        //foreach (SkinnedMeshRenderer renderer in childrenRenderers)
+        //{
+        //    Debug.Log(renderer.material.color.a);
+        //    renderer.material.SetFloat("_OutlineOpacity", 0);
+        //}
+    //}
+}
