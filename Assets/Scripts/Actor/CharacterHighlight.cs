b527cbe63880dac428a0c0d2943cb9382b6235fd
fatclarence <ckoh.clarence.96@gmail.com>
2020-02-06 11:20:38 +0800

    maintains current code changes

--- Assets/Scripts/Actor/CharacterHighlight.cs
+++ Assets/Scripts/Actor/CharacterHighlight.cs
@@ -5,41 +5,54 @@ using UnityEngine;
 public class CharacterHighlight : MonoBehaviour
 {
     private Renderer[] m_Renderers;
-    // Start is called before the first frame update
+    private bool ran;
     void Start()
+    {
+        GetRenderers();
+        ran = false;
+
+    }
+
+    private void Update()
+    {
+        if (!ran)
+        {
+            CheckRenderers();
+            ran = true;
+        }
+        
+
+    }
+
+    void GetRenderers()
     {
         m_Renderers = this.gameObject.GetComponentsInChildren<Renderer>();
     }
 
-
-    private void Update()
-    {        
+    void CheckRenderers()
+    {
+        List<Material> materials = new List<Material>();
+        List<Material> sharedMaterials = new List<Material>();
         foreach (Renderer r in m_Renderers)
         {
-            if (r == null)
+            if (r != null)
             {
-                continue;
-            } 
-            else
-            {
-                Material[] materials = r.GetComponents<Material>();
-                if (materials != null)
+                r.GetMaterials(materials);
+                r.GetSharedMaterials(sharedMaterials);
+                foreach (Material m in materials)
                 {
-                    foreach (Material m in materials)
+                    if (m != null)
                     {
-                        if (m != null)
-                        {
-                            m.SetFloat("_OutlineOpacity", 1);
-                        }
+                        Debug.Log(m.name);
+                        m.SetColor("_OutlineColor", Color.red);
+                        r.material = m;
                     }
                 }
-                else
-                {
-                    break;
-                }
+                
             }
         }
     }
+
     //private void SetMaterialOpacity()
     //{
         //SkinnedMeshRenderer[] childrenRenderers = m_Player.GetComponentsInChildren<SkinnedMeshRenderer>();
