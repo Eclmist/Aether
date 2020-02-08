67e32b7f4066a7ec2333a60be8a9520e8267cf4f
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-02-09 00:38:50 +0800

    Rename IInteractor to I_CanInteract

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -9,7 +9,7 @@ public abstract class PowerUp : MonoBehaviour, IInteractable
         AudioManager.m_Instance.PlaySound("MAGIC_Powerup", 1.0f, 1.2f);
     }
 
-    public void Interact(IInteractor interactor) 
+    public void Interact(I_CanInteract interactor) 
     {
         if (interactor != null) 
         {
