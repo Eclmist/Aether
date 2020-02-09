4e1df71024c53fbc55ae626714d85c0d3f580b23
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-02-09 12:17:12 +0800

    Update Logic for Powerups and PlayerHandler

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public abstract class PowerUp : MonoBehaviour, IInteractable
 {
-    protected void PlayPickUpSound() 
+    private void PlayPickUpSound() 
     {
         AudioManager.m_Instance.PlaySound("MAGIC_Powerup", 1.0f, 1.2f);
     }
 
     public void Interact(ICanInteract interactor) 
     {
-        if (interactor != null) 
+        if (interactor != null && interactor is PlayerHandler) 
         {
+            PlayPickUpSound();
+            HandlePowerups(((PlayerHandler) interactor).GetPlayerPowerupActor());
             Destroy(gameObject);
         }
     }
+
+    public abstract void HandlePowerups(PowerupActor powerupActor);
+    
 }
