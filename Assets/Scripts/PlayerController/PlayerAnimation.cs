e89f63c24fd90415c4f9a60b5925d3fb302c9828
tcgj <terenceblur0410@gmail.com>
2020-02-11 02:48:47 +0800

    Clean up requirecomponent

--- Assets/Scripts/PlayerController/PlayerAnimation.cs
+++ Assets/Scripts/PlayerController/PlayerAnimation.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerMovement), typeof(Player))]
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(PlayerMovement))]
 public class PlayerAnimation : MonoBehaviour
 {
     [SerializeField]
