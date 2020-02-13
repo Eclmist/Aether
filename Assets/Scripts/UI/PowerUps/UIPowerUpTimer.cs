6244b8409b7187a61e1a5d370e5cd278851606e8
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-02-13 22:12:40 +0800

    Add Test UI Elements

---
+++ Assets/Scripts/UI/PowerUps/UIPowerUpTimer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPowerUpTimer : MonoBehaviour
+{
+    [SerializeField]
+    private Image m_Filler;
+
+    [SerializeField]
+    private Image m_Icon;
+
+    private Color m_WhiteColor = new Color(255, 255, 255, 100);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Filler.fillAmount > 0.0f) 
+        {
+            m_Filler.fillAmount -= (0.20f * Time.deltaTime);
+        } 
+
+        if (m_Filler.fillAmount >= 0.5f)
+        {
+            m_Icon.color = m_Filler.color;
+        }
+        else if (m_Filler.fillAmount > 0.0f) {
+            FlipIconColors();
+        }
+        else {
+            m_Icon.color = m_WhiteColor;
+        }
+    }
+
+    private void FlipIconColors() 
+    {
+        if (m_Icon.color == m_WhiteColor) 
+        {
+            m_Icon.color = m_Filler.color;
+        }
+        else {
+            m_Icon.color = m_WhiteColor;
+        }
+    }
+}
