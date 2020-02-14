b41dffe492a31a24a7c090777a3ed0bdc4394710
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-02-14 12:36:42 +0800

    Explore fadeOut and blinks

--- Assets/Scripts/UI/PowerUps/UIPowerUpTimer.cs
+++ Assets/Scripts/UI/PowerUps/UIPowerUpTimer.cs
@@ -11,15 +11,19 @@ public class UIPowerUpTimer : MonoBehaviour
     [SerializeField]
     private Image m_Icon;
 
-    private Color m_WhiteColor = new Color(255, 255, 255, 100);
-
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
+    {
+        UpdateIcon();
+    }
+
+    private void UpdateIcon()
     {
         if (m_Filler.fillAmount > 0.0f) 
         {
@@ -28,24 +32,48 @@ public class UIPowerUpTimer : MonoBehaviour
 
         if (m_Filler.fillAmount >= 0.5f)
         {
-            m_Icon.color = m_Filler.color;
+            SetColoredIcon();
         }
         else if (m_Filler.fillAmount > 0.0f) {
-            FlipIconColors();
+            FadeIcon();
         }
         else {
-            m_Icon.color = m_WhiteColor;
+            SetWhiteIcon();
         }
     }
 
-    private void FlipIconColors() 
+    private void SetWhiteIcon() 
     {
-        if (m_Icon.color == m_WhiteColor) 
+        m_Icon.color = new Color(255, 255, 255, m_Icon.color.a);
+    }
+
+    private void SetWhiteIcon(float alpha) 
+    {
+        m_Icon.color = new Color(255, 255, 255, alpha);
+    }
+
+    private void SetColoredIcon()
+    {
+        m_Icon.color = new Color(m_Filler.color.r, m_Filler.color.g, m_Filler.color.b, m_Icon.color.a);
+    }
+
+    private void SetColoredIcon(float alpha)
+    {
+        m_Icon.color = new Color(m_Filler.color.r, m_Filler.color.g, m_Filler.color.b, alpha);
+    }
+
+    private void FadeIcon() 
+    {
+        float newAlpha = m_Icon.color.a - (0.40f * Time.deltaTime);
+
+        if (m_Icon.color.r == 255 && m_Icon.color.g == 255 && m_Icon.color.r == 255) 
         {
-            m_Icon.color = m_Filler.color;
+            SetColoredIcon(newAlpha);
         }
         else {
-            m_Icon.color = m_WhiteColor;
+            SetWhiteIcon(newAlpha);
         }
+
+        Debug.Log(m_Icon.color.r + " : " + m_Icon.color.a);
     }
 }
