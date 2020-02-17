2fad7b28bffd7460e59a7881241f0c49644bba85
heze8 <46834785+heze8@users.noreply.github.com>
2020-02-17 20:39:36 +0800

    More changes

--- Assets/Scripts/Gameplay/ItemSkills/ProjectileSkill.cs
+++ Assets/Scripts/Gameplay/ItemSkills/ProjectileSkill.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ProjectileSkill : ItemSkill
 {
