f8de8f843cb4cf29d5ab18fd95bd587f92896d33
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-02-17 17:57:23 +0800

    Integrate Player actions

---
+++ Assets/Scripts/Utility/BitmaskHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BitmaskHelper
+{
+    public static bool AnyBitSet<T>(T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        return (flagsValue & flagValue) != 0;
+    }
+
+    public static bool AllBitSet<T>(T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        return (flagsValue & flagValue) == flagValue;
+    }
+
+    public static bool NoBitSet<T>(T flags, T flag) where T : struct
+    {
+        int flagsValue = (int)(object)flags;
+        int flagValue = (int)(object)flag;
+
+        return (flagsValue & flagValue) == 0;
+    }
+}
