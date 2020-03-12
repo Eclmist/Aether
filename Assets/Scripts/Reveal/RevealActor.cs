7f8ae5364498c3d244a2473674df9ad545ca6b4c
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-03-12 20:07:51 +0800

    Add Reveal utility scripts

--- Assets/Scripts/Reveal/RevealActor.cs
+++ Assets/Scripts/Reveal/RevealActor.cs
@@ -6,9 +6,8 @@ public class RevealActor : MonoBehaviour
 {
     [SerializeField]
     private float m_Radius = 5;
-
-    [SerializeField]
-    private float m_RadiusModifierForVisibilityMgr = 0.5f;
+    private float m_RadiusModifier = 1;
+    private float m_RadiusModifierTarget = 1;
 
     [SerializeField]
     private LayerMask m_ObjectLayerMask = new LayerMask();
@@ -19,15 +18,16 @@ public class RevealActor : MonoBehaviour
     {
         m_VisibilityModifier = new VisibilityManager.VisibilityModifier();
         m_VisibilityModifier.m_Position = transform.position;
-        m_VisibilityModifier.m_Radius = m_Radius * m_RadiusModifierForVisibilityMgr;
+        m_VisibilityModifier.m_Radius = GetRadius();
         VisibilityManager.Instance.RegisterPersistentVisibility(m_VisibilityModifier);
     }
 
     // Update the terrain per vertex. 
     private void Update()
     {
+        m_RadiusModifier = Mathf.Lerp(m_RadiusModifier, m_RadiusModifierTarget, Time.deltaTime);
         // Update Revealable Objects (the ones that fade from bottom to top in one go)
-        Collider[] colliders = Physics.OverlapSphere(transform.position, m_Radius, m_ObjectLayerMask);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, GetRadius(), m_ObjectLayerMask);
 
         foreach (Collider c in colliders)
         {
@@ -40,12 +40,29 @@ public class RevealActor : MonoBehaviour
         }
 
         m_VisibilityModifier.m_Position = transform.position;
-        m_VisibilityModifier.m_Radius = m_Radius;
+        m_VisibilityModifier.m_Radius = GetRadius();
 
         if (Input.GetKeyDown(KeyCode.N))
         {
             m_VisibilityModifier.m_IsUnreveal = !m_VisibilityModifier.m_IsUnreveal;
         }
+
+        ResetRadiusModifier();
+    }
+
+    private float GetRadius()
+    {
+        return m_Radius * m_RadiusModifier;
+    }
+
+    public void SetRadiusModifier(float mod)
+    {
+        m_RadiusModifierTarget = Mathf.Max(mod, m_RadiusModifierTarget);
+    }
+
+    public void ResetRadiusModifier()
+    {
+        m_RadiusModifierTarget = 1;
     }
 
     private void OnDrawGizmos()
