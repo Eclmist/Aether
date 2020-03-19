8fdd4aae1883c98b11825117df590e1017ccec30
Low Jun Kai, Sean <sean.low.jk@gmail.com>
2020-03-19 20:06:07 +0800

    Add Basic Structure for Popup Menu

---
+++ Assets/Scripts/UI/Menus/MenusManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenusManager : Singleton<MenusManager>
+{
+    [SerializeField]
+    private TriggerableAnimator[] m_Animators;
+
+    public void TriggerAnimator(int index)
+    {
+        if (m_Animators == null || index >= m_Animators.Length)
+            return;
+
+        TriggerableAnimator animator = m_Animators[index];
+        animator.TriggerAnimation();
+    }
+
+    public bool GetTriggerBool(int index)
+    {
+         if (m_Animators == null || index >= m_Animators.Length)
+            throw new UnityException("Should not call on null or out of bounds");
+
+        TriggerableAnimator animator = m_Animators[index];
+        return animator.GetTriggerBool();
+    }
+}
