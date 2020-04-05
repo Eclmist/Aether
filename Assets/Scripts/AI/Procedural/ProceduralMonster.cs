ebe589318dd5b7719c43de10af8b460bf84179bc
heze8 <46834785+heze8@users.noreply.github.com>
2020-04-05 20:52:19 +0800

    Add procedural monster script
    (cherry picked from commit 0029f6099fe62c57b884e419b1abb94bf85f354f)

---
+++ Assets/Scripts/AI/Procedural/ProceduralMonster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+class ProceduralMonster : MonoBehaviour
+{
+    [SerializeField] 
+    public bool ChangeType = false;
+    [SerializeField]
+    private int type = 0;
+
+    private void Awake()
+    {
+        if (ProceduralMonsterSpawner.HasInstance)
+        {
+            ProceduralMonsterSpawner.Instance.AddSpawnedMonster(this);
+        }
+    }
+
+    public void OnValidate()
+    {
+        if (ChangeType && ProceduralMonsterSpawner.HasInstance)
+        {
+            ProceduralMonsterSpawner.Instance.SpawnMonsterAtLocation(transform.position, type);
+            Destroy(gameObject);
+        }
+
+        ChangeType = false;
+    }
+ }
