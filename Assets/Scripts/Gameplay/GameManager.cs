f88bd55ea98eadf50da1b93acbfa2fb504a7d212
tcgj <terenceblur0410@gmail.com>
2020-04-13 01:14:50 +0800

    Add respawn system when player falls off

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -6,20 +6,46 @@ public class GameManager : Singleton<GameManager>
 {
     public event System.Action<GameMode> GameStarted;
 
+    [SerializeField]
+    private TowerBase[] m_TowerCheckpoints;
+
     private float m_GameOverMessageDuration = 10f;
 
     private bool m_GameStarted = false;
 
     private Transform m_RespawnPoint;
 
+    private float m_RespawnHeight = -50f;
+
     private void Awake()
     {
         PlayerManager.Instance.GetPlayerNetworkManager().AllPlayersReady += StartGame;
+
+        if (m_TowerCheckpoints != null)
+        {
+            foreach (TowerBase tower in m_TowerCheckpoints)
+                tower.TowerCaptured += EnableCheckpoint;
+        }
+    }
+
+    private void Update()
+    {
+        if (!m_GameStarted)
+            return;
+
+        Player player = PlayerManager.Instance.GetLocalPlayer();
+
+        if (player.transform.position.y < m_RespawnHeight)
+            player.TriggerRespawn();
     }
 
     public void StartGame()
     {
-        PlayerManager.Instance.GetPlayerNetworkManager().AllPlayersReady -= StartGame;
+        PlayerNetworkManager pnm = PlayerManager.Instance.GetPlayerNetworkManager();
+        PlayerDetails details = PlayerManager.Instance.GetLocalPlayer().GetPlayerDetails();
+        pnm.AllPlayersReady -= StartGame;
+        m_RespawnPoint = pnm.GetSpawnPosition(details.GetPosition());
+
         Debug.Log("Game started");
         m_GameStarted = true;
         GameStarted?.Invoke(GameMode.GAMEMODE_ZOOM_RACING_CIRCUIT_BREAKER);
@@ -44,6 +70,14 @@ public class GameManager : Singleton<GameManager>
     public bool GetGameStarted()
     {
         return m_GameStarted;
+    }
+
+    public void EnableCheckpoint(TowerBase tower)
+    {
+        Debug.Log("Checkpoint tower captured");
+        tower.TowerCaptured -= EnableCheckpoint;
+        Checkpoint checkpoint = tower.GetComponent<Checkpoint>();
+        checkpoint.enabled = true;
     }
 
     public Transform GetRespawnPoint()
