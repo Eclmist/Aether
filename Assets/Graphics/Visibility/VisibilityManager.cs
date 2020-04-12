fc22b0a81d2ae09a9a3fdc0027aec46be4e08c03
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-04-13 03:29:01 +0800

    Fix visibility not resetting between scenes

--- Assets/Graphics/Visibility/VisibilityManager.cs
+++ Assets/Graphics/Visibility/VisibilityManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VisibilityManager : Singleton<VisibilityManager>
 {
@@ -39,6 +40,23 @@ public class VisibilityManager : Singleton<VisibilityManager>
         LoadCS();
         InitGlobalTextures();
         SetShaderGlobals();
+
+        SceneManager.sceneLoaded += (Scene scn, LoadSceneMode mode) =>
+        {
+            if (m_VisibilityCS == null)
+                return;
+
+            if (m_WorldVisibilityTexture == null)
+                return;
+
+            int numThreadGroupX = m_TextureSizeX / THREADGROUP_SIZE_X;
+            int numThreadGroupY = m_TextureSizeY / THREADGROUP_SIZE_Y;
+            m_VisibilityCS.SetTexture(RESET_KERNEL, "WorldVisibilityResult", m_WorldVisibilityTexture);
+            m_VisibilityCS.Dispatch(RESET_KERNEL, numThreadGroupX, numThreadGroupY, 1);
+
+            m_PersistentModifierList.Clear();
+            m_OneShotModifierQueue.Clear();
+        };
     }
 
     private void OnDestroy()
