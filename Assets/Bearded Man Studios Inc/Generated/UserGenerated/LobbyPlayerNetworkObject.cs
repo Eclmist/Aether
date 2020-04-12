59c8ff9b6c761b94f445c64afd0c20464fdbfb5c
tcgj <terenceblur0410@gmail.com>
2020-04-12 17:12:57 +0800

    Set up ready button in lobby

--- Assets/Bearded Man Studios Inc/Generated/UserGenerated/LobbyPlayerNetworkObject.cs
+++ Assets/Bearded Man Studios Inc/Generated/UserGenerated/LobbyPlayerNetworkObject.cs
@@ -8,7 +8,7 @@ namespace BeardedManStudios.Forge.Networking.Generated
 	[GeneratedInterpol("{\"inter\":[]")]
 	public partial class LobbyPlayerNetworkObject : NetworkObject
 	{
-		public const int IDENTITY = 17;
+		public const int IDENTITY = 6;
 
 		private byte[] _dirtyFields = new byte[0];
 
