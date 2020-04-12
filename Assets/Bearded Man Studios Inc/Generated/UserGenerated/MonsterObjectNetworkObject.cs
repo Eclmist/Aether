1a570f57b8af0d0fb6fcae21e0278e412efb1c04
heze8 <46834785+heze8@users.noreply.github.com>
2020-04-12 23:42:03 +0800

    Network

--- Assets/Bearded Man Studios Inc/Generated/UserGenerated/MonsterObjectNetworkObject.cs
+++ Assets/Bearded Man Studios Inc/Generated/UserGenerated/MonsterObjectNetworkObject.cs
@@ -8,7 +8,7 @@ namespace BeardedManStudios.Forge.Networking.Generated
 	[GeneratedInterpol("{\"inter\":[0,0.17,0.15]")]
 	public partial class MonsterObjectNetworkObject : NetworkObject
 	{
-		public const int IDENTITY = 17;
+		public const int IDENTITY = 9;
 
 		private byte[] _dirtyFields = new byte[1];
 
