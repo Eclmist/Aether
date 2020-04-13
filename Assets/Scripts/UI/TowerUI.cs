6debf4e8017cd78e19b1c288a793c1c33ec6e923
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-04-13 13:26:31 +0800

    tower fills script

---
+++ Assets/Scripts/UI/TowerUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TowerUI : MonoBehaviour
+{
+    public GameObject ui;
+    public Image fill;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.Instance.GetCurrentTower() == null)
+        {
+            ui.SetActive(false);
+        }
+
+        ui.SetActive(true);
+        fill.fillAmount = GameManager.Instance.GetCurrentTower().GetCapturePercentage();
+    }
+}
