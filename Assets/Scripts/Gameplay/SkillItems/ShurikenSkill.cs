169c9760a409d01f60d6e731ebfc5a63d43890ce
fatclarence <ckoh.clarence.96@gmail.com>
2020-04-15 02:04:29 +0800

    fix shuriken and tornado travel towards aim point

--- Assets/Scripts/Gameplay/SkillItems/ShurikenSkill.cs
+++ Assets/Scripts/Gameplay/SkillItems/ShurikenSkill.cs
@@ -4,13 +4,23 @@ using BeardedManStudios.Forge.Networking.Unity;
 
 public class ShurikenSkill : SkillItem
 {
+    [SerializeField]
+    private LayerMask m_LayerMask;
     public override void UseSkill(Transform playerTransform)
     {
-        Vector3 startSpawnPosition = playerTransform.position + new Vector3(0.0f, 1.2f, 0.0f);
-        Vector3 playerForward = playerTransform.forward.normalized;
-        Vector3 spellOffset = playerForward * 1.5f;
-        startSpawnPosition += spellOffset;
-        NetworkManager.Instance.InstantiateSkills(index: 6, position: startSpawnPosition, rotation: Quaternion.LookRotation(playerForward));
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, m_LayerMask))
+        {
+            Vector3 startSpawnPosition = playerTransform.position 
+                + 25.0f * (playerTransform.forward.normalized)
+                + new Vector3(0.0f, 1.6f, 0.0f);
+            Vector3 hitPoint = hit.point - playerTransform.position;
+            //Vector3 spellOffset = playerForward * 1.5f;
+            //startSpawnPosition += spellOffset;
+            NetworkManager.Instance.InstantiateSkills(index: 6, position: startSpawnPosition, rotation: Quaternion.LookRotation(hitPoint, playerTransform.up));
+        }
+        
     }
 
 }
