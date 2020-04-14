7351ef11a081a9790dde213e664840b4bc7e8488
fatclarence <ckoh.clarence.96@gmail.com>
2020-04-15 02:12:46 +0800

    remove commented code

--- Assets/Scripts/Gameplay/SkillItems/TornadoSkillItem.cs
+++ Assets/Scripts/Gameplay/SkillItems/TornadoSkillItem.cs
@@ -15,8 +15,6 @@ public class TornadoSkillItem : SkillItem
         {
             Vector3 startSpawnPosition = playerTransform.position + 5.0f * (playerTransform.forward.normalized);
             Vector3 hitPoint = hit.point - playerTransform.position;
-            //Vector3 spellOffset = playerForward * 1.5f;
-            //startSpawnPosition += spellOffset;
             NetworkManager.Instance?.InstantiateSkills(index: 4, position: startSpawnPosition, rotation: Quaternion.LookRotation(hitPoint, playerTransform.up));
         }
 
